01e7d6944272d5fb362ac60ced84c7f05b9513d7
Ssssoar <jano20@vtr.net>
2023-10-22 20:56:50 -0400

    arrow
    took way too many effort

---
+++ Assets/Scripts/TurnToRB.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnToRB : MonoBehaviour{
+    public Rigidbody rb;
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (rb == null) {return;}
+        if (rb.constraints == RigidbodyConstraints.FreezeAll) {return;}
+        transform.forward = rb.velocity;
+
+    }
+}
